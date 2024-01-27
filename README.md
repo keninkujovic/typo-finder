7405bfc6b835b6c473a21fdf32c97615639b78ed
keninkujovic <kujovic.kenin@gmail.com>
2024-01-27 17:48:45 +0100

    FEATURE: Add call counter

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 Notes:
+https://www.starlette.io/middleware/#basehttpmiddleware
 
 Damerau-Levenshtein Distance
 
 https://www.geeksforgeeks.org/ternary-search-tree/
-
-
--- server.py
+++ server.py
@@ -1,8 +1,19 @@
+import aioredis
 from fastapi import FastAPI
-from pydantic import BaseModel
 from fastapi.responses import HTMLResponse
+from pydantic import BaseModel
+from starlette.middleware.base import BaseHTTPMiddleware
 
 from main import TST, findTypos, loadDictionary
+
+redis = aioredis.from_url("redis://redis", encoding="utf-8", decode_responses=True)
+
+class CounterMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request, call_next):
+        response = await call_next(request)
+        response = await call_next(request)
+        await redis.incr("total_api_calls")
+        return response
 
 class Text(BaseModel):
     text: str
@@ -10,9 +21,15 @@ class Text(BaseModel):
 app = FastAPI()
 tst = TST()
 
+app.add_middleware(CounterMiddleware)
+
 @app.on_event("startup")
 async def startup_event():
     loadDictionary(tst, "dictionary.txt")
+
+@app.get("/total-calls/")
+async def total_calls():
+    return await redis.get("total_api_calls")
 
 @app.post("/api/typo-check/")
 async def post(body: Text):
