dc3aa11d9b2e77a2a88311e0e9dda86f9123be0f
keninkujovic <kujovic.kenin@gmail.com>
2024-01-28 18:36:51 +0100

    FIX: Rename parameters

--- sym.py
+++ sym.py
@@ -1,18 +1,18 @@
 from symspellpy import SymSpell, Verbosity
 
 
-def findTypos(sym_spell, text, max_suggestions_per_word=3):
-    unique_words = set(text.split())
+def findTypos(sym_spell, text, max_suggestions=3):
+        unique_words = set(text.split())
 
-    corrected_text = []
+        corrected_text = []
 
-    for word in unique_words:
-        suggestions = sym_spell.lookup(word, Verbosity.CLOSEST)
+        for word in unique_words:
+            suggestions = sym_spell.lookup(word, Verbosity.CLOSEST)
 
-        suggestions = suggestions[:max_suggestions_per_word]
+            suggestions = suggestions[:max_suggestions]
 
-        corrected_words = set(suggestion.term for suggestion in suggestions)
+            corrected_words = set(suggestion.term for suggestion in suggestions)
 
-        corrected_text.append(corrected_words)
+            corrected_text.append(corrected_words)
 
-    return corrected_text
+        return corrected_text
