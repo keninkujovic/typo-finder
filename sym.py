0b2013793c17851819a2f511a47e545a8808a8cd
keninkujovic <kujovic.kenin@gmail.com>
2024-01-30 00:00:27 +0100

    FIX: Change to dict

--- sym.py
+++ sym.py
@@ -4,15 +4,13 @@ from symspellpy import SymSpell, Verbosity
 def findTypos(sym_spell, text, max_suggestions=3):
         unique_words = set(text.split())
 
-        corrected_text = []
+        corrected_text = {}
 
         for word in unique_words:
             suggestions = sym_spell.lookup(word, Verbosity.CLOSEST)
-
             suggestions = suggestions[:max_suggestions]
 
-            corrected_words = set(suggestion.term for suggestion in suggestions)
-
-            corrected_text[word].append(corrected_words)
+            corrected_words = {suggestion.term for suggestion in suggestions}
+            corrected_text[word] = list(corrected_words)
 
         return corrected_text
